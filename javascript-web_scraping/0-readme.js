a3737d7a8a597f66d0a9a894d850e9cc41f1e6a4
goever1 <30449167+goever1@users.noreply.github.com>
2023-09-22 12:07:40 -0500

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const FileName = process.argv[2];
-fs.readFile(FileName, 'utf8', function (err, line) {
+const file = process.argv[2];
+fs.readFile(file, 'utf8', function (err, line) {
   if (err) {
     console.log(err);
   } else {
